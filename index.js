566571736e63ccf8039c7a2a0535e5507797466c
endtrastic <a.tuvikene@voco.ee>
2024-09-23 11:16:25 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const key  = 'c96c8ff678b4a66e9a03560cd6a4515d'
 let city = 'Tartu'
@@ -16,7 +18,6 @@ app.get('/', function (req, res) {
       return response.json()
     })
     .then((data) => {
-        console.log(data)
         let description = data.weather[0].description
         let city = data.name
         let temp = Math.round(parseFloat(data.main.temp) - 273.15)
@@ -27,5 +28,23 @@ app.get('/', function (req, res) {
         })
     })
 })
+
+app.post('/', function (req, res) {
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(response => {
+      return response.json()
+    })
+    .then((data) => {
+      let description = data.weather[0].description
+      let city = data.name
+      let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+      res.render('index', {
+        description: description,
+        city: city,
+        temp: temp
+          })
+      })
+  })
 
 app.listen(8080)
